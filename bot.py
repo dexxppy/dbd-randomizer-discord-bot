794dfc7d83017622287301e240ddc95a19bbac7e
dexxppy <dex.a6310@gmail.com>
2026-07-31 14:22:25 +0200

    Remove asyncio exception handler

--- bot.py
+++ bot.py
@@ -1,47 +1,27 @@
-import traceback
+import discord
+import os
 
-try:
-    import discord
-    import os
+from discord.ext import commands
+from dotenv import load_dotenv
 
-    from discord.ext import commands
-    from dotenv import load_dotenv
+from utils.error_handler import *
+from utils.commands import register_commands, register_help_command, register_character_specific_commands
+from utils.events import register_events
 
-    from utils.error_handler import *
-    from utils.commands import (
-        register_commands,
-        register_help_command,
-        register_character_specific_commands,
-    )
-    from utils.events import register_events
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
-    intents = discord.Intents.default()
-    intents.message_content = True
-    intents.guilds = True
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+CHANNEL_ID = int(os.getenv('LOG_CHANNEL_ID'))
 
-    bot = commands.Bot(
-        command_prefix="!",
-        intents=intents,
-        help_command=None
-    )
+register_help_command(bot)
+register_character_specific_commands(bot)
+register_commands(bot, "killer")
+register_commands(bot, "survivor")
+register_events(bot, CHANNEL_ID)
 
-    load_dotenv()
+bot.run(TOKEN)
 
-    TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-    CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
-
-    print("TOKEN:", TOKEN is not None)
-    print("CHANNEL:", CHANNEL_ID)
-
-    register_help_command(bot)
-    register_character_specific_commands(bot)
-    register_commands(bot, "killer")
-    register_commands(bot, "survivor")
-    register_events(bot, CHANNEL_ID)
-
-    print("Starting bot...")
-    bot.run(TOKEN)
-
-except Exception:
-    traceback.print_exc()
-    raise
